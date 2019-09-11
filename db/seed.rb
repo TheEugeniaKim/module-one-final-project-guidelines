78cd300a162c58fc27125d038384dcab9580c77e
github username <github email address>
2019-09-11 16:49:53 -0400

    seed file setup

--- db/seed.rb
+++ db/seed.rb
@@ -1,26 +1,46 @@
-# Test branching
+#### Create Book Instances
 
-Book.create(title: "Harry Potter 1", author: "JK Rowling", fiction_nonfiction: "fiction", 
+Book.destroy_all
+
+hp1 = Book.create(title: "Harry Potter 1", author: "JK Rowling", fiction_nonfiction: "fiction", 
 genre: "fantasy", publication_year: 1997)
 
-Book.create(title: "Harry Potter 2", author: "JK Rowling", fiction_nonfiction: "fiction", 
-genre: "fantasy", publication_year: 1998)
-
-Book.create(title: "Harry Potter 3", author: "JK Rowling", fiction_nonfiction: "fiction", 
-genre: "fantasy", publication_year: 1999)
-
-Book.create(title: "Harry Potter 4", author: "JK Rowling", fiction_nonfiction: "fiction", 
-genre: "fantasy", publication_year: 2000)
-
-Book.create(title: "Harry Potter 5", author: "JK Rowling", fiction_nonfiction: "fiction", 
-genre: "fantasy", publication_year: 2003)
-
-Book.create(title: "Harry Potter 6", author: "JK Rowling", fiction_nonfiction: "fiction", 
+hp6 = Book.create(title: "Harry Potter 6", author: "JK Rowling", fiction_nonfiction: "fiction", 
 genre: "fantasy", publication_year: 2005)
 
-Book.create(title: "Harry Potter 7", author: "JK Rowling", fiction_nonfiction: "fiction", 
+hp7 = Book.create(title: "Harry Potter 7", author: "JK Rowling", fiction_nonfiction: "fiction", 
 genre: "fantasy", publication_year: 2007)
 
-Patron.create(name: "Eugenia", birthdate: "2000-03-04")
+lr1 = Book.create(title: "Lord of the Rings 1", author: "J.R.R. Tolkien", fiction_nonfiction: "fiction", 
+genre: "fantasy", publication_year: 1954)
 
-Loan.create(patron_id: 1, book_id: 1)
+lr2 = Book.create(title: "Lord of the Rings 2", author: "J.R.R. Tolkien", fiction_nonfiction: "fiction", 
+genre: "fantasy", publication_year: 1954)
+
+fsg = Book.create(title: "Fifty Shades of Grey", author: "E.L. James", fiction_nonfiction: "fiction", 
+genre: "erotic fantasy", publication_year: 2011)
+
+am = Book.create(title: "The Autobiography of Malcolm X", author: "Malcolm X", fiction_nonfiction: "nonfiction", 
+genre: "biography", publication_year: 1965)
+
+ggs = Book.create(title: "Guns, Germs, and Steel", author: "Malcolm X", fiction_nonfiction: "nonfiction", 
+genre: "general nonfiction", publication_year: 1997)
+
+#### Create Patron Instances
+
+Patron.destroy_all
+
+eugenia = Patron.create(name: "Eugenia", birthdate: "1994-04-07")
+nick = Patron.create(name: "Nick", birthdate: "1991-09-05")
+bill = Patron.create(name: "Bill", birthdate: "2000-01-01")
+cindy = Patron.create(name: "Cindy", birthdate: "1995-12-12")
+
+#### Create Loan Instances
+
+Loan.destroy_all
+
+Loan.create(patron_id: eugenia.id, book_id: hp1.id)
+Loan.create(patron_id: eugenia.id, book_id: lr2.id)
+Loan.create(patron_id: nick.id, book_id: am.id)
+Loan.create(patron_id: bill.id, book_id: hp7.id)
+
